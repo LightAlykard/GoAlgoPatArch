7dcc9156d00619afec6787d3268f1819b7fbaa65
LightAlykard <lightalykard@yandex.ru>
2021-02-16 19:45:28 +0300

    DZ-2
    DZ-2

---
+++ shop_new/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"	
+	"gb/algoandtd/hw/shop_new/notification"
+	"gb/algoandtd/hw/shop_new/repository"
+	"gb/algoandtd/hw/shop_new/service"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var tokenStr string
+	flag.StringVar(&tokenStr, "t", "", "token for telegram api")
+
+	flag.Parse()
+
+	notif, err := notification.NewTelegramBot(tokenStr, 323615875)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	from := "service@list.ru"
+	password := "passd"
+	host := "smtp.list.ru"
+	port := "587"
+
+	smtpBot := notification.NewSMTPBot(from, host, port, password)
+
+	rep := repository.NewMapDB()
+	service := service.NewService(rep, notif, *smtpBot)
+	s := &server{
+		service: service,
+		rep:     rep,
+		smtpBot: *smtpBot,
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/items", s.listItemHandler).Methods("GET")
+	router.HandleFunc("/items", s.createItemHandler).Methods("POST")
+	router.HandleFunc("/items/{id}", s.getItemHandler).Methods("GET")
+	router.HandleFunc("/items/{id}", s.deleteItemHandler).Methods("DELETE")
+	router.HandleFunc("/items/{id}", s.updateItemHandler).Methods("PUT")
+
+	router.HandleFunc("/orders", s.listOrdersHandler).Methods("GET")
+	router.HandleFunc("/orders", s.createOrderHandler).Methods("POST")
+
+	srv := &http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ shop_new/notification/notification.go
@@ -0,0 +1,37 @@
+package notification
+
+import (
+	"fmt"
+	"gb/algoandtd/hw/shop_new/models"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Notification interface {
+	SendOrderCreated(order *models.Order) error
+}
+
+type telegramBot struct {
+	chatID int64
+	tgBot  *tgbotapi.BotAPI
+}
+
+func (s *telegramBot) SendOrderCreated(order *models.Order) error {
+	text := fmt.Sprintf("new order %d\n\nphone: %s\nemail: %s", order.ID, order.CustomerPhone, order.CustomerEmail)
+
+	fmt.Println(s.chatID)
+	msg := tgbotapi.NewMessage(s.chatID, text)
+
+	_, err := s.tgBot.Send(msg)
+	return err
+}
+
+func NewTelegramBot(token string, chatID int64) (Notification, error) {
+	tgBot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return nil, err
+	}
+	return &telegramBot{
+		chatID: chatID,
+		tgBot:  tgBot,
+	}, nil
+}
