0c25c5acc9d769d01f25f13c0f7fa4ef85eed853
LightAlykard <lightalykard@yandex.ru>
2021-02-26 14:07:34 +0300

    HW5

---
+++ DZ5/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type Node struct {
+	next  *Node
+	prev  *Node
+	Data int 
+}
+
+type List struct {
+	len int 
+	head  *Node
+	tail  *Node
+}
+
+func (l *List) Print() {
+	for tmp, i := l.head, l.len; i > 0; tmp, i = tmp.next, i-1 {
+		fmt.Printf("Value %d, Prev: %v, Next: %v \n", tmp.Data, tmp.prev, tmp.next)
+	}
+
+}
+
+func (l *List) Len() {
+	return l.len
+	fmt.Printf("Length %d \n", l.len)
+}
+
+func (l *List) Find(elem int) *Node {
+	if l.head != nil {
+		for tmp := l.head; tmp != l.tail; tmp = tmp.next {
+			if tmp.Data == elem {
+				fmt.Printf("Element find: Value %d, Prev %v, Next %v \n", tmp.Data, tmp.prev, tmp.next)
+				break
+			}
+		}
+	}
+	fmt.Println("Element not find")	
+}
+
+func (l *List) PushBack(node *Node) {
+	prev := l.tail
+	l.len++
+
+	if l.head == nil {
+		l.head = node
+		l.tail = node
+		return
+	}
+
+	prev.next = node
+	node.prev = prev
+	l.tail = node
+}
+
+func (l *List) PushFront(node *Node) {
+
+	prev := l.head
+
+	l.len++
+
+	if l.head == nil {
+		l.head = node
+		l.tail = node
+		return
+	}
+
+	node.next = prev
+	prev.prev = node
+	l.head = node
+
+}
+
+func (l *List) PopBack() {
+
+	if l.len <= 0 {
+		log.Fatal("PopBack() called on empty queue")
+	}
+
+	del := l.tail
+	node := del.prev
+	node.next = nil
+	del.prev = nil
+	l.tail = node
+	l.len--
+}
+
+func (l *List) PopFront() {
+	if l.len <= 0 {
+		log.Fatal("PopFront() called on empty queue")
+	}
+
+	del := l.head
+	head := l.head.next
+
+	head.prev = nil
+	del.next = nil
+	l.head = head
+	l.len--
+}
+
+func main() {
+	l := &List{}
+
+	node1 := &Node{Data: 1}
+	node2 := &Node{Data: 2}
+	node3 := &Node{Data: 3}
+	node4 := &Node{Data: 4}
+
+	fmt.Println("PushBack")
+	l.PushBack(node1)
+	l.PushBack(node2)
+	l.PushBack(node3)
+
+	l.Print()
+
+	fmt.Println("\nPushFront")
+	l.PushFront(node4)
+
+	l.Print()
+
+	l.Find(4)
+
+	fmt.Println("\nPopBack")
+
+	l.PopBack()
+	l.Print()
+
+	fmt.Println("\nPopFront")
+
+	l.PopFront()
+	l.Print()
+
+}
